6149b1796026a267fa807f0fc5f910074eabc8f6
rohitkumar21 <rohit.kumar21@nagarro.com>
2023-11-20 14:53:42 +0530

    first commit

---
+++ src/main/java/com/example/demo/JenkinsCicdDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsCicdDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsCicdDockerApplication.class, args);
+	}
+
+}
